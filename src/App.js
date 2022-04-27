ce915dc070b30ee26ddcc72c60fe5b37b30b18f3
aren0416 <aren0416@gmail.com>
2022-04-27 21:08:13 +0900

    basic routes setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import { routes } from "./routes";
+import { Home } from "./screens/Home";
+import { Login } from "./screens/Login";
+
 function App() {
-  return <div></div>;
+  const isLoggedIn = false;
+
+  return (
+    <Router>
+      <Routes>
+        <Route path={routes.home} element={isLoggedIn ? <Home /> : <Login />} />
+        {/* <Route path={routes.signup} element="" /> */}
+        <Route path="/*" element={""} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
